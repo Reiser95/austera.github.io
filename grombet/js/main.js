d172f5059f75c53891cbbc3b64792e40b539f337
Reiser95 <elmanamirkhanov@ail.ru>
2019-09-29 20:12:42 +0500

    grombet

--- grombet/js/main.js
+++ grombet/js/main.js
@@ -16,14 +16,123 @@ $(document).ready(function(){
 	var freeInner = $(".inner__free");
 
 
-	var match = $(".match__inner");
-	var fadeInner = $(".fade__inner");
+	var match = $(".inner__mobile--menu");
+
 
 	match.on("click", function(){
-		fadeInner.fadeToggle(100);
+		$(this).find(".fade__inner").fadeToggle(100);
+	});
+
+	var select = $(".select");
+
+	select.on("click", function(){
+		if($(this).hasClass("active")){
+			$(this).removeClass("active");
+			$(this).find(".select__inner").fadeOut(100);
+		}
+		else if(!$(this).hasClass("active")){
+			$(".select__inner").fadeOut(100);
+			$(this).addClass("active");
+			$(this).find(".select__inner").fadeIn(100);
+		}
+	});
+
+	$(".select__item").on("click", function(){
+		$(".select__inner").fadeOut(100);
 	});
 
 
+	var dataset = [
+	  {
+	    value: 15,
+	    color: '#00cf7f',
+	    profit: 10
+	  }, {
+	    value: 5,
+	    color: '#4895fa',
+	    profit: 100
+	  }, {
+	    value: 15,
+	    color: '#8379c7',
+	    profit: 1000
+	  }, {
+	    value: 20,
+	    color: '#ff8396',
+	    profit: 1100
+	  }, {
+	    value: 5,
+	    color: '#21cdcb',
+	    profit: 1200
+	  }, {
+	    value: 10,
+	    color: '#663cd3',
+	    profit: 1500
+	  }, {
+	    value: 5,
+	    color: '#cd7221',
+	    profit: 2000
+	  }, {
+	    value: 5,
+	    color: '#ff3d5a',
+	    profit: 10000000
+	  },{
+	    value: 25,
+	    color: '#ffa16b',
+	    profit: 121200
+	  }
+	];
+
+	var maxValue = 25;
+	var container = $('.circle__item');
+	 
+	var addSector = function(data, startAngle, collapse) {
+	  var sectorDeg = 3.6 * data.value;
+	  var skewDeg = 90 + sectorDeg;
+	  var rotateDeg = startAngle;
+	  if (collapse) {
+	    skewDeg++;
+	  }
+	 
+	  var sector = $('<div>', {
+	    'class': 'sector',
+	    "data-color": data.color,
+	    "data-value": data.profit
+	  }).css({
+	    'background': data.color,
+	    'transform': 'rotate(' + rotateDeg + 'deg) skewY(' + skewDeg + 'deg)'
+	  });
+	  container.append(sector);
+	 
+	  return startAngle + sectorDeg;
+	};
+	 
+	dataset.reduce(function (prev, curr) {
+	  return (function addPart(data, angle) {
+	    if (data.value <= maxValue) {
+	      return addSector(data, angle, false);
+	    }
+	 
+	    return addPart({
+	      value: data.value - maxValue,
+	      color: data.color
+	    }, addSector({
+	      value: maxValue,
+	      color: data.color,
+	    }, angle, true));
+	  })(curr, prev);
+	}, 0);
+
+	$(".sector").on("click", function(){
+	  $(".text__inner").css("display", "flex");
+	  var color = $(this).attr("data-color");
+	  var value = $(this).attr("data-value");
+	  $(".text__inner").css("color", color);
+	  $(".fill__svg").css("fill", color);
+	  $(".profit__value").text(value);
+	});
+
+
+	
 
 	checkbox.on("click", function(){
 		money.toggleClass("checkbox__grey");
@@ -57,8 +166,8 @@ $(document).ready(function(){
 		decimalPlaces: 0,
 		decimalPoint: '1000',
 		thousandSeparator: ',',
-		duration: 1000, // ms; TOTAL length animation
-		leeway: 50, // percent of duraion
+		duration: 1000,
+		leeway: 50,
 		easing: 'spincrementEasing',
 		fade: true
 	});
@@ -69,8 +178,8 @@ $(document).ready(function(){
 		decimalPlaces: 0,
 		decimalPoint: '1000',
 		thousandSeparator: ',',
-		duration: 1000, // ms; TOTAL length animation
-		leeway: 50, // percent of duraion
+		duration: 1000,
+		leeway: 50,
 		easing: 'spincrementEasing',
 		fade: true
 	});
@@ -81,8 +190,8 @@ $(document).ready(function(){
 		decimalPlaces: 0,
 		decimalPoint: '1000',
 		thousandSeparator: ',',
-		duration: 1000, // ms; TOTAL length animation
-		leeway: 50, // percent of duraion
+		duration: 1000,
+		leeway: 50,
 		easing: 'spincrementEasing',
 		fade: true
 	});
