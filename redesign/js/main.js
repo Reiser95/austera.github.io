882326bd4682d5ef2d1d2a8e4adf8ec721ac4fd4
Reiser95 <elmanamirkhanov@ail.ru>
2019-11-02 22:49:58 +0500

    redesign

---
+++ redesign/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	/*=== Переключение языков ===*/ 
+
+	let flag = $(".header__top--language--flag");
+
+	flag.on("click", function(){
+		if($(this).hasClass("active__flag")){
+			return;
+		}
+		else{
+			flag.removeClass("active__flag");
+			$(this).addClass("active__flag");
+		}
+	});
+
+	/*=== Модальное окно соглашения ===*/
+
+	let agree = $(".user__agree");
+
+	agree.on("click", function(e){
+		e.preventDefault();
+	});
+
+});
+
+$(window).on("load",function (){
+
+  $("#preloader").fadeOut(500);
+
+});
