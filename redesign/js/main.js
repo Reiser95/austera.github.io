f667a11dfc86c527eea33cc5f9e7e6bbcfa71bae
Reiser95 <elmanamirkhanov@ail.ru>
2019-11-04 21:41:24 +0500

    redesign

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+	var html = document.querySelector("html");
+
+	if(localStorage.getItem("gamma") != ""){
+		html.style.setProperty("--blueC", localStorage.getItem("gamma"));
+	}
+
 	/*=== Переключение языков ===*/ 
 
 	let flag = $(".header__top--language--flag");
@@ -33,7 +39,7 @@ $(document).ready(function(){
 	});
 
 
-	/* Слайдеры */
+	/*=== Слайдеры ===*/
 
 	let gameSlider = $(".main__like--games--carousel");
 
@@ -50,13 +56,53 @@ $(document).ready(function(){
 	    gameSlider.trigger('prev.owl.carousel');
 	});
 
+	/*=== Цветовая гамма ===*/ 
+
+	let chooseColor = $(".header__top--choose--color");
+	let colorMask = $(".choose__color--inner");
+	let colorClose = $(".choose__color--cross");
+
+	chooseColor.on("click", function(){
+		colorMask.fadeIn(300).css("display", "flex");
+	});
+
+	colorClose.on("click", function(){
+		colorMask.fadeOut(300);
+	});
+
+
+	/*=== Переключение цветовой гаммы ===*/ 
+
+	let color = $(".choise__color--circle");
+
+	let chooseBtn = $(".choose__color--button");
+
+	/* При клике на цвет давать активный класс */ 
+
+	color.on("click", function(){
+		if(!$(this).hasClass("choose__color--active")){
+			color.removeClass("choose__color--active");
+			$(this).addClass("choose__color--active");
+		}
+	});
+
+	/* При клике на "Выбрать", выбирать соответствующий цвет */ 
+
+	chooseBtn.on("click", function(){
+		var colorVar = $(".choose__color--active").css("color");
+		html.style.setProperty("--blueC", colorVar);
+		localStorage.setItem("gamma", colorVar);
+
+	});
+
+	/* Сохраняем выбранный цвет гаммы в куки */
+
+	
 
 });
 
 /*=== Прелоадер ===*/ 
 
 $(window).on("load",function (){
-
   $("#preloader").fadeOut(500);
-
 });
