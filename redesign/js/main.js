46c59e2469e49da9a13b6fc6896b2e767f8cdde0
Reiser95 <elmanamirkhanov@ail.ru>
2019-11-06 21:56:46 +0500

    redesign

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+	/*=== Слайдеры ===*/
+
+	let gameSlider = $(".main__like--games--carousel");
+
+	gameSlider.owlCarousel({
+		items: 3,
+		autoWidth: true,
+		margin: 35
+	});
+
+	$('.main__arrow--next').click(function () {
+	    gameSlider.trigger('next.owl.carousel');
+	});
+	$('.main__arrow--prev').click(function () {
+	    gameSlider.trigger('prev.owl.carousel');
+	});
+
 	/* Проверяем наличие выбранной гаммы */ 
 
 	var html = document.querySelector("html");
@@ -10,14 +27,21 @@ $(document).ready(function(){
 
 	var add;
 
+	/* Проверяем если гамма пустая, то ничего не делать */ 
+
 	if(localStorage.getItem("gamma") != ""){
 		html.style.setProperty("--blueC", localStorage.getItem("gamma"));
 	}
+
+	/* Проверяем если цвет гаммы пустой, то ничего не делать */ 
 
 	if(localStorage.getItem("checkcolor")){
 		color.removeClass("choose__color--active");
 		$("#"+localStorage.getItem("checkcolor")+"").addClass("choose__color--active");
 	}
+
+	/* Проверяем есть ли активные игры, что бы дальше пройтись
+	   массивом и дать классы активным играм */ 
 
 	if((localStorage.getItem("activegames"))){
 		var arr = (localStorage.getItem("activegames"));
@@ -30,6 +54,12 @@ $(document).ready(function(){
 
 		for(let o of nameActiveGame){
 			$(".add__like--item:contains("+o+")").addClass("active__like--game");
+
+			/* Вывод в слайдер любимых игр */ 
+
+			gameSlider
+			.trigger('add.owl.carousel', ['<div class="main__like--games--item"><a href="#" class="game__inner--mask">'+o+'</a></div>', -1])
+			.trigger('refresh.owl.carousel');
 		}
 	}
 
@@ -65,24 +95,6 @@ $(document).ready(function(){
 		$(".chat__content").toggleClass("chat__off");
 		$(this).toggleClass("chat__button--on");
 		$(".chat__close--icon").toggleClass("chat__close--icon--rotate");
-	});
-
-
-	/*=== Слайдеры ===*/
-
-	let gameSlider = $(".main__like--games--carousel");
-
-	gameSlider.owlCarousel({
-		items: 3,
-		autoWidth: true,
-		margin: 35
-	});
-
-	$('.main__arrow--next').click(function () {
-	    gameSlider.trigger('next.owl.carousel');
-	});
-	$('.main__arrow--prev').click(function () {
-	    gameSlider.trigger('prev.owl.carousel');
 	});
 
 	/*=== Цветовая гамма ===*/ 
@@ -152,27 +164,61 @@ $(document).ready(function(){
 			$(this).removeClass("active__like--game");
 			$(this).children(".add__like--plus").fadeIn(0);
 
+			/* Удаляем циклом из локалки игру которую удалили
+			   пользователи */ 
+
 			add = $(this).children(".game__inner--mask").text().trim();
 
 			for(let s = 0; s<nameActiveGame.length; s++){
 				if(nameActiveGame[s] == add){
+					/* Имя удаленного элемента */ 
+
+					let deleteName = nameActiveGame[s];
+
+					/* Удаление из активного элемента */
+
+					let value = -1;
+					
+					$(".owl-item").each(function(i){
+						value ++;
+						if($(this).text() == deleteName){
+							gameSlider
+							.trigger('remove.owl.carousel', [value])
+							.trigger('refresh.owl.carousel');
+						}
+					});
+
 					nameActiveGame.splice(s, 1);
 				}
 			}
 
-			/* Сохранение в локалку имя активной игры */
+			/* Сохранение в локалку имя активных игр */
 
 			localStorage.setItem("activegames", nameActiveGame);
+
+
+
+			
 		}
 		else{
 			$(this).children(".add__like--plus").fadeOut(0);
 			$(this).addClass("active__like--game");
+
+			/* Создаем переменную add что бы проверять ей
+			   на наличие выбранной любимой игры */ 
+
 			add = $(this).children(".game__inner--mask").text().trim();
 			nameActiveGame.push(add);
 			
-			/* Сохранение в локалку имя активной игры */
+			/* Сохранение в локалку имя активных игр */
 
 			localStorage.setItem("activegames", nameActiveGame);
+
+			/* Добавление в слайдер активного элемента */
+
+			gameSlider
+			.trigger('add.owl.carousel', ['<div class="main__like--games--item"><a href="#" class="game__inner--mask">'+add+'</a></div>', -1])
+			.trigger('refresh.owl.carousel');
 		}
 	});
 
