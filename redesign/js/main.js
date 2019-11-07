47ed3b74a16c71fb4afdfde92f3ca7aae706d637
Reiser95 <elmanamirkhanov@ail.ru>
2019-11-07 17:53:30 +0500

    redesign

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
 
+	/* Функция смены объектов местами */
+
+	jQuery.fn.swap = function(b) {
+	    b = jQuery(b)[0];
+	    var a = this[0],
+	        a2 = a.cloneNode(true),
+	        b2 = b.cloneNode(true),
+	        stack = this;
+
+	    a.parentNode.replaceChild(b2, a);
+	    b.parentNode.replaceChild(a2, b);
+
+	    stack[0] = a2;
+	    return this.pushStack( stack );
+	};
+
 	/*=== Слайдеры ===*/
 
 	let gameSlider = $(".main__like--games--carousel");
 
 	gameSlider.owlCarousel({
-		items: 3,
-		autoWidth: true,
-		margin: 35
+		margin: 35,
+		responsiveClass:true,
+	    /* responsive:{
+	        0:{
+	            items:1
+	        },
+	        768:{
+	            items:2
+	        },
+		    1200:{
+		        items:3,
+	        }
+		} */
 	});
 
 	$('.main__arrow--next').click(function () {
@@ -27,13 +53,15 @@ $(document).ready(function(){
 
 	var add;
 
-	/* Проверяем если гамма пустая, то ничего не делать */ 
+	/* Проверяем если гамма не пустая,
+	   то менять цвет гаммы на выбранный */ 
 
 	if(localStorage.getItem("gamma") != ""){
 		html.style.setProperty("--blueC", localStorage.getItem("gamma"));
 	}
 
-	/* Проверяем если цвет гаммы пустой, то ничего не делать */ 
+	/* Проверяем если цвет гаммы не пустой,
+	   то менять цвет выбора гаммы на соответствующий */ 
 
 	if(localStorage.getItem("checkcolor")){
 		color.removeClass("choose__color--active");
@@ -58,7 +86,7 @@ $(document).ready(function(){
 			/* Вывод в слайдер любимых игр */ 
 
 			gameSlider
-			.trigger('add.owl.carousel', ['<div class="main__like--games--item"><a href="#" class="game__inner--mask">'+o+'</a></div>', -1])
+			.trigger('add.owl.carousel', ['<div class="main__like--games--item like__game"><a href="#" class="game__inner--mask">'+o+'</a><div class="edit__mask"><div class="edit__arrows"><i class="fas fa-chevron-circle-left edit__arrow edit__arrow--prev"></i><i class="fas fa-chevron-circle-right edit__arrow edit__arrow--next"></i></div></div></div>', -1])
 			.trigger('refresh.owl.carousel');
 		}
 	}
@@ -146,9 +174,20 @@ $(document).ready(function(){
 	let addGames = $(".add__like--games");
 	let addMask = $(".add__like--game");
 	let addCross = $(".add__like--cross");
+	let edit = $(".main__button--edit");
 
 	addGames.on("click", function(){
 		addMask.fadeIn(300).css("display", "flex");
+
+		/* Проверяется, если пользователь хочет добавить
+		   игру в избранные при активном редактировании,
+		   то редактирование выключается */ 
+		if(edit.hasClass("edit")){
+			edit.removeClass("edit");
+			$(".main__like--games--item").unbind("touchstart mousedown");
+
+			$(".main__like--games--item").removeClass("active__edit");
+		}
 	});
 
 	addCross.on("click", function(){
@@ -160,6 +199,9 @@ $(document).ready(function(){
 	let game = $(".add__like--item");
 
 	game.on("click", function(){
+		/* Проверяем активная ли игра на которую мы нажали,
+		   если да, то забираем активный класс */ 
+
 		if($(this).hasClass("active__like--game")){
 			$(this).removeClass("active__like--game");
 			$(this).children(".add__like--plus").fadeIn(0);
@@ -195,11 +237,11 @@ $(document).ready(function(){
 			/* Сохранение в локалку имя активных игр */
 
 			localStorage.setItem("activegames", nameActiveGame);
-
-
-
 			
 		}
+
+		/* Если игра не активная, даем ей активный класс */ 
+
 		else{
 			$(this).children(".add__like--plus").fadeOut(0);
 			$(this).addClass("active__like--game");
@@ -217,8 +259,95 @@ $(document).ready(function(){
 			/* Добавление в слайдер активного элемента */
 
 			gameSlider
-			.trigger('add.owl.carousel', ['<div class="main__like--games--item"><a href="#" class="game__inner--mask">'+add+'</a></div>', -1])
+			.trigger('add.owl.carousel', ['<div class="main__like--games--item like__game"><a href="#" class="game__inner--mask">'+add+'</a><div class="edit__mask"><div class="edit__arrows"><i class="fas fa-chevron-circle-left edit__arrow edit__arrow--prev"></i><i class="fas fa-chevron-circle-right edit__arrow edit__arrow--next"></i></div></div></div>', -1])
 			.trigger('refresh.owl.carousel');
+		}
+	});
+
+	/* Нажатие на кнопку редактирования */
+
+	edit.on("click", function(){
+		if($(this).hasClass("edit")){
+			/* Даем класс что бы в последующем определить элемент */
+			$(this).removeClass("edit");
+
+			/* Забираем класс активного редактирования,
+			   если класс drag есть */
+			$(".main__like--games--item").removeClass("active__edit");
+		}
+		else{
+			/* Даем класс что бы в последующем определить элемент */ 
+			$(this).addClass("edit");
+
+			/* Даем класс активного редактирования,
+			   если класса drag нет */
+			$(".main__like--games--item").addClass("active__edit");
+		}
+	});
+
+	/* Нажатие на стрелочки перемещения в редактировании */
+
+	console.log(nameActiveGame);
+
+	$(document).on("click", ".edit__arrow--next" ,function(){
+		var rightValue = $(this).parents(".edit__arrows").parents(".edit__mask").siblings(".game__inner--mask").text();
+		
+		for(let u = 0; u<nameActiveGame.length; u++){
+			if(nameActiveGame[u] == rightValue){
+
+				/* Временная переменная помогающая нам поменять
+				   в будущем переменные массива местами */ 
+				var tem = nameActiveGame[u + 1];
+
+				/* Находим совпавший элемент и его соседа справа */ 
+				let leftSwap = $(".game__inner--mask:contains("+nameActiveGame[u]+")").parent(".like__game");
+				let rightSwap = $(".game__inner--mask:contains("+nameActiveGame[u + 1]+")").parent(".like__game");
+
+				/* Меняем элементы местами */ 
+				leftSwap.swap(rightSwap);
+
+				/* Заменяем поменяные местами элементы массива */ 
+				nameActiveGame[u + 1] = nameActiveGame[u];
+				nameActiveGame[u] = tem;
+
+				/* Удаляем старый массив и сохраняем новый(обновленный) */
+				localStorage.removeItem("activegames");
+				localStorage.setItem("activegames", nameActiveGame);
+
+				/* Если находим совпадение в массиве, останавливаем его */ 
+				break;
+			}
+		}
+	});
+
+	$(document).on("click", ".edit__arrow--prev" ,function(){
+		var leftValue = $(this).parents(".edit__arrows").parents(".edit__mask").siblings(".game__inner--mask").text();
+		
+		for(let w = 0; w<nameActiveGame.length; w++){
+			if(nameActiveGame[w] == leftValue){
+
+				/* Временная переменная помогающая нам поменять
+				   в будущем переменные массива местами */ 
+				var ver = nameActiveGame[w - 1];
+
+				/* Находим совпавший элемент и его соседа слева */ 
+				let leftSwap2 = $(".game__inner--mask:contains("+nameActiveGame[w]+")").parent(".like__game");
+				let rightSwap2 = $(".game__inner--mask:contains("+nameActiveGame[w - 1]+")").parent(".like__game");
+				
+				/* Меняем элементы местами */
+				rightSwap2.swap(leftSwap2);
+
+				/* Заменяем поменяные местами элементы массива */
+				nameActiveGame[w - 1] = nameActiveGame[w];
+				nameActiveGame[w] = ver;
+
+				/* Удаляем старый массив и сохраняем новый(обновленный) */ 
+				localStorage.removeItem("activegames");
+				localStorage.setItem("activegames", nameActiveGame);
+
+				/* Если находим совпадение в массиве, останавливаем его */
+				break;
+			}
 		}
 	});
 
