c8bf7f733042bac81c6994ef657ff4b08440e85a
Reiser95 <elmanamirkhanov@ail.ru>
2019-11-12 22:32:56 +0500

    menu

--- menu/menu_files/js.js
+++ menu/menu_files/js.js
@@ -20,8 +20,33 @@ jQuery(document).ready(function ($) {
     $(window).resize(function(){
       if($(window).width() >= 751){
         $(".modal__mobile--slide").slideUp(0);
+        $(".click__menu").slideUp(0);
       }
     });
+
+    $(".popup_close1").on("click", function(){
+      $(".popup_box1").fadeOut(300);
+    });
+
+    $(".open__popup").on("click", function(){
+      $(".popup_box1").fadeIn(300).css("display", "flex");
+    });
+
+    $('.button__up').on('click', function(e){
+      $('html,body').stop().animate({ scrollTop: $('#point').offset().top }, 500);
+      e.preventDefault();
+    });
+
+    $('.up').on('click', function(e){
+      $('html,body').stop().animate({ scrollTop: $('#point').offset().top }, 500);
+      e.preventDefault();
+    });
+
+    $(".modal__mobile--item--continue").on("click", function(){
+      $(".click__menu").slideToggle(100).css("display", "flex");
+    });
+
+
 
     if($('input[type=radio][name=payment]:checked').val() == 'PayKeeper'){
         $('input[type=checkbox][name=payment_bn]').attr('hidden', false);
