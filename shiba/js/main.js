6d50af02b4af0e30a18a93d6b499717796fbb124
Reiser95 <elmanamirhanov@mail.ru>
2021-08-24 11:52:23 +0500

    shiba

--- shiba/js/main.js
+++ shiba/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	const close = () => {
+		$(".menu").removeClass("active");
+		$(".menu").attr("src", "img/menu.svg");
+		$("body").removeClass("scroll");
+		$(".modal__menu").removeClass("active");
+	}
+
+	const check = () => {
+		let s_top = $("html").scrollTop();
+		let yes = $(".total__value").offset().top;
+
+		if(s_top > yes - 800 && s_top < yes){
+		    $(".total__value").spincrement({
+		    	to: 1000000000000,
+		    	duration: 2000
+		    });
+		}
+	}
 
 	$(".faq__item").on('click', function(){
 		$(this).children('.faq__item--title').toggleClass('active');
@@ -24,16 +42,22 @@ $(document).ready(function(){
 
 	$(window).resize(function(){
 		if($(window).width() > 998){
-			$(".menu").removeClass("active");
-			$(".menu").attr("src", "img/menu.svg");
-			$("body").removeClass("scroll");
-			$(".modal__menu").removeClass("active");
+			close();
 		}
 	});
 
-	$(".total__value").spincrement({
-		to: 1000000000000,
-		duration: 2000
+	$(".ankor__link").on('click', function(e){
+		e.preventDefault();
+		let href = $(this).attr('data-point');
+		let top = $("#"+href).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+		close();
 	});
+
+	$(document).scroll(function(){
+	    check();
+	});
+
+	check();
 
 });
