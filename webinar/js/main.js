aec78480ec95ba90e7084d8e5bffaf5adaf85c17
Reiser95 <elmanamirhanov@mail.ru>
2021-09-25 15:56:38 +0500

    webinar

---
+++ webinar/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear(), 10 - 1, 16);
+	$('.timer').countdown({until: austDay, labels: ['Лет', 'Месяцев', 'Недель', 'Дней', 'Часов', 'Минут', 'Секунд']});
+
+});
