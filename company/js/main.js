bc94ef571b1c2f20c0838a71abc34cdba658c428
Reiser95 <elmanamirhanov@mail.ru>
2021-10-04 22:12:22 +0500

    company

---
+++ company/js/main.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+	const close = () => {
+		$("body").removeClass("scroll");
+		$(".modal__menu").removeClass("active");
+	}
+
+	$(".burger").on('click', function(){
+		$("body").addClass("scroll");
+		$(".modal__menu").addClass("active");
+	});
+
+	$(".modal__menu--close").on('click', function(){
+		close();
+	});
+
+	$(window).resize(function(){
+		if($(window).width() > 998){
+			close();
+		}
+	});
+
+	const giveActiveClass = (thisElement, element) => {
+		if(!thisElement.hasClass('active')){
+			thisElement.siblings("."+element).removeClass('active');
+			thisElement.addClass('active');
+		}
+	}
+
+	$(".rec__item--button").on('click', function(){
+		giveActiveClass($(this), "rec__item--button");
+	});
+
+	$(".city__wrapper--item").on('click', function(){
+		giveActiveClass($(this), "city__wrapper--item");
+	});
+
+	// Карусели
+	$(".store__content").slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<div class="store__arrow store__arrow--prev"><img src="img/prev.png" alt="Стрелка" class="store__arrow--icon"></div>',
+		nextArrow: '<div class="store__arrow store__arrow--next"><img src="img/next.png" alt="Стрелка" class="store__arrow--icon"></div>',
+		appendArrows: '.arrows__wrapper',
+		responsive: [
+		    {
+		        breakpoint: 1870,
+		        settings: {
+			        slidesToShow: 3
+		        }
+		    },
+		    {
+		        breakpoint: 998,
+		        settings: {
+			        slidesToShow: 2
+		        }
+		    },
+		    {
+		        breakpoint: 768,
+		        settings: {
+			        slidesToShow: 1
+		        }
+		    }
+		]
+	});
+
+	$(".main__inner").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="main__arrow main__arrow--prev"><img src="img/prev.png" alt="Стрелка" class="main__arrow--icon"></div>',
+		nextArrow: '<div class="main__arrow main__arrow--next"><img src="img/next.png" alt="Стрелка" class="main__arrow--icon"></div>'
+	});
+
+});
