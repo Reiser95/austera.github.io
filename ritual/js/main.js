c00d69cbd50fab3379b3dbf283007af906a76027
Reiser95 <elmanamirhanov@mail.ru>
2022-01-30 15:37:30 +0500

    ritual

--- ritual/js/main.js
+++ ritual/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
 
-	
+	$(".form").on("click", function(){
+		let top = $("#form").offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+
+	$(".send").on("click", function(){
+		let name = $(".name").val().trim();
+		let email = $(".email").val().trim();
+		let phone = $(".phone").val().trim();
+
+		if(!name){
+			alert("Введите ваше имя!");
+		}
+		else if(!email){
+			alert("Введите адрес электронной почты!")
+		}
+		else if(!phone){
+			alert("Введите номер телефон!")
+		}
+		else{
+			$.ajax({
+				method: "POST",
+				url: "mail.php",
+				data: {
+					name: name,
+					email: email,
+					phone: phone
+				},
+				success: function(){
+					alert("Заявка успешно отправлена!");
+				}
+			});
+		}
+	});
 
 });
